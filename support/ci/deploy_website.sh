96d643119461a43ef317bd1430d6ee9ad26b429c
Josh Black <raskchanky@gmail.com>
2016-11-22 09:14:02 -0800

    Deploy www.habitat.sh when a PR merges to master
    Signed-off-by: Josh Black <raskchanky@gmail.com>

--- support/ci/deploy_website.sh
+++ support/ci/deploy_website.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
 
-
-git log HEAD~1..HEAD | grep -q '!!! Temporary Commit !!!'
-is_tmp_commit=$?
-
 set -eu
 
-cd www
-./bin/middleman build
-
-# If we are not on a pull request, on the "auto" branch (which homu uses when
-# auto-merging master), and not on a temporary commit, then run the publish
-# script.
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ] &&
-   [ "${TRAVIS_BRANCH}" = "auto" ] &&
-   [[ $is_tmp_commit = 1 ]]; then
-  ./bin/middleman s3_sync
-  curl -H "Fastly-Key: ${FASTLY_API_KEY}" -X POST "https://api.fastly.com/service/${FASTLY_SERVICE_KEY}/purge_all"
-else echo "Not on master; skipping website deploy"; fi
+# if [[ "${TRAVIS_PULL_REQUEST}" = "false" ]] && [[ "${TRAVIS_BRANCH}" = "master" ]]; then
+if [[ "${TRAVIS_PULL_REQUEST}" = "false" ]] && [[ "${TRAVIS_BRANCH}" = "jb/build-web-site-again-lol" ]]; then
+  cd www
+  make deploy
+fi
